5eb9d1ecacf3305fca82d78db2c7a48a49a1ce07
James Park <james.a.park@live.com>
2015-08-04 15:58:19 +0100

    First Commit

---
+++ acf-signature/acf-signature-v4.php
@@ -0,0 +1,343 @@
+<?php
+
+class acf_field_signature extends acf_field
+{
+	// vars
+	var $settings, // will hold info such as dir / path
+		$defaults; // will hold default field options
+
+
+	/*
+	*  __construct
+	*
+	*  Set name / label needed for actions / filters
+	*
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function __construct()
+	{
+		// vars
+		$this->name = 'signature';
+		$this->label = __('Signature');
+		$this->category = __("Basic",'acf'); // Basic, Content, Choice, etc
+		$this->defaults = array(
+			// add default here to merge into your field.
+			// This makes life easy when creating the field options as you don't need to use any if( isset('') ) logic. eg:
+			//'preview_size' => 'thumbnail'
+		);
+
+
+		// do not delete!
+    parent::__construct();
+
+
+    // settings
+		$this->settings = array(
+			'path' => apply_filters('acf/helpers/get_path', __FILE__),
+			'dir' => apply_filters('acf/helpers/get_dir', __FILE__),
+			'version' => '1.0.0'
+		);
+
+	}
+
+
+	/*
+	*  create_options()
+	*
+	*  Create extra options for your field. This is rendered when editing a field.
+	*  The value of $field['name'] can be used (like bellow) to save extra data to the $field
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field	- an array holding all the field's data
+	*/
+
+	function create_options($field)
+	{
+		
+	}
+
+
+	/*
+	*  create_field()
+	*
+	*  Create the HTML interface for your field
+	*
+	*  @param	$field - an array holding all the field's data
+	*
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function create_field( $field )
+	{
+		// create Field HTML
+		?>
+			<div class="acf-input-wrap">
+				<div id="signature-pad" class="m-signature-pad">
+					<input type="hidden" name="<?php echo $field['name']; ?>" value="<?php echo $field['value']; ?>" />
+					<div class="m-signature-pad--body">
+						<canvas></canvas>
+					</div>
+					<div class="m-signature-pad--footer">
+						<?php /** / ?><div class="m-signature-pad--description">Sign above</div><?php /**/ ?>
+						<a href="#clear" class="m-signature-pad--clear btn btn-default btn-xs button button-small" data-action="clear">Clear</a>
+						<?php /** / ?><button class="save" data-action="save">Save</button><?php /**/ ?>
+					</div>
+				</div>
+			</div>
+		<?php
+	}
+
+
+	/*
+	*  input_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is created.
+	*  Use this action to add css + javascript to assist your create_field() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_enqueue_scripts()
+	{
+		// register script libraries
+		#wp_register_script('flashcanvas', $this->settings['dir'] . 'js/libs/flashcanvas.js', '', $this->settings['version']);
+		wp_register_script('signaturepad', $this->settings['dir'] . 'js/libs/signature_pad.min.js', '', $this->settings['version']);
+
+		// register acf scripts
+		wp_register_script('acf-input-signature', $this->settings['dir'] . 'js/input.js', array('acf-input','signaturepad'), $this->settings['version']);
+		$args = array(
+			'is_mobile' => wp_is_mobile()
+		);
+		wp_localize_script('acf-input-signature', 'signature', $args);
+		wp_register_style('acf-input-signature', $this->settings['dir'] . 'css/input.css', array('acf-input'), $this->settings['version']);
+
+		// scripts
+		wp_enqueue_script(array(
+			'acf-input-signature',
+		));
+
+		// styles
+		wp_enqueue_style(array(
+			'acf-input-signature',
+		));
+
+	}
+
+
+	/*
+	*  input_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is created.
+	*  Use this action to add css and javascript to assist your create_field() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function input_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  field_group_admin_enqueue_scripts()
+	*
+	*  This action is called in the admin_enqueue_scripts action on the edit screen where your field is edited.
+	*  Use this action to add css + javascript to assist your create_field_options() action.
+	*
+	*  $info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_enqueue_scripts
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_enqueue_scripts()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  field_group_admin_head()
+	*
+	*  This action is called in the admin_head action on the edit screen where your field is edited.
+	*  Use this action to add css and javascript to assist your create_field_options() action.
+	*
+	*  @info	http://codex.wordpress.org/Plugin_API/Action_Reference/admin_head
+	*  @type	action
+	*  @since	3.6
+	*  @date	23/01/13
+	*/
+
+	function field_group_admin_head()
+	{
+		// Note: This function can be removed if not used
+	}
+
+
+	/*
+	*  load_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value found in the database
+	*  @param	$post_id - the $post_id from which the value was loaded from
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the value to be saved in te database
+	*/
+
+	function load_value($value, $post_id, $field)
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+
+
+	/*
+	*  update_value()
+	*
+	*  This filter is appied to the $value before it is updated in the db
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value - the value which will be saved in the database
+	*  @param	$post_id - the $post_id of which the value will be saved
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$value - the modified value
+	*/
+
+	function update_value($value, $post_id, $field)
+	{
+		// Note: This function can be removed if not used
+		return $value;
+	}
+
+
+	/*
+	*  format_value()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed to the create_field action
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+	function format_value($value, $post_id, $field)
+	{
+		// defaults?
+		/*
+		$field = array_merge($this->defaults, $field);
+		*/
+
+		// perhaps use $field['preview_size'] to alter the $value?
+
+
+		// Note: This function can be removed if not used
+		return $value;
+	}
+
+
+	/*
+	*  format_value_for_api()
+	*
+	*  This filter is appied to the $value after it is loaded from the db and before it is passed back to the api functions such as the_field
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$value	- the value which was loaded from the database
+	*  @param	$post_id - the $post_id from which the value was loaded
+	*  @param	$field	- the field array holding all the field options
+	*
+	*  @return	$value	- the modified value
+	*/
+
+	function format_value_for_api($value, $post_id, $field)
+	{
+		// If not empty, build an image, otherwise return an empty string
+		if ( $value && is_string($value) )
+		{
+			$value = '<img src="' . $value . '" alt="Signature" />';
+		}
+		return $value;
+	}
+
+
+	/*
+	*  load_field()
+	*
+	*  This filter is appied to the $field after it is loaded from the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*
+	*  @return	$field - the field array holding all the field options
+	*/
+
+	function load_field($field)
+	{
+		return $field;
+	}
+
+
+	/*
+	*  update_field()
+	*
+	*  This filter is appied to the $field before it is saved to the database
+	*
+	*  @type	filter
+	*  @since	3.6
+	*  @date	23/01/13
+	*
+	*  @param	$field - the field array holding all the field options
+	*  @param	$post_id - the field group ID (post_type = acf)
+	*
+	*  @return	$field - the modified field
+	*/
+
+	function update_field($field, $post_id)
+	{
+		// Note: This function can be removed if not used
+		return $field;
+	}
+
+
+}
+
+
+// create field
+new acf_field_signature();
+
+?>
---
+++ acf-signature/acf-signature.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+Plugin Name: Advanced Custom Fields: Signature
+Plugin URI: http://jamespark.ninja/plugins/
+Description: Adds a signature field to ACF, based on SignaturePad by Szimek & ACFv4 work by http://Jupitercow.com/
+Version: 1.0
+Author: James Park
+Author URI: http://jamespark.ninja
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+*/
+
+
+
+
+// 1. set text domain
+// Reference: https://codex.wordpress.org/Function_Reference/load_plugin_textdomain
+//load_plugin_textdomain( 'acf-signature', false, dirname( plugin_basename(__FILE__) ) . '/lang/' ); 
+
+
+
+
+// 2. Include field type for ACF5
+// $version = 5 and can be ignored until ACF6 exists
+function include_field_types_signature( $version ) {
+	
+	include_once('acf-signature-v5.php');
+	
+}
+
+add_action('acf/include_field_types', 'include_field_types_signature');	
+
+
+
+
+// 3. Include field type for ACF4
+function register_fields_signature() {
+	
+	include_once('acf-signature-v4.php');
+	
+}
+
+add_action('acf/register_fields', 'register_fields_signature');	
+
+
+
+	
+?>
